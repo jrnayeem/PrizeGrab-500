855c951637497b24a9e948f8e707603e49cc4887
jrnayeem <126013512+jrnayeem@users.noreply.github.com>
2026-09-18 15:57:52 +0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,22 +7,22 @@
 
 export const CTA_URLS: Record<string, string> = {
   // Header CHECK ELIGIBILITY button (desktop and mobile versions use this same link)
-  header_cta: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=TM01',
+  header_cta: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM01',
 
   // Main hero CHECK ELIGIBILITY button
-  hero_section: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=TM02',
+  hero_section: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM02',
 
   // Featured promotion CHECK ELIGIBILITY button
-  featured_promo: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=TM03',
+  featured_promo: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM03',
 
   // Offer card CHECK ELIGIBILITY button
-  offer_card: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social',
+  offer_card: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM04',
 
   // What You Need To Do CHECK ELIGIBILITY button
-  what_you_need_checklist: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social',
+  what_you_need_checklist: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM05',
 
   // Final CHECK ELIGIBILITY button
-  final_cta: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social',
+  final_cta: 'https://linkthem.net/aff_c?offer_id=4837&aff_id=201949&source=social&sub1=TM06',
 };
 
 /**
